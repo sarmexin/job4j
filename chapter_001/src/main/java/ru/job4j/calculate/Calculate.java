8b23b613770fa9f189afbb529793b8d24af46418
sarmexin <mailto:sarmexin@gmail.com>
2018-08-10 13:01:27 +0300

    1.5. Checkstyle

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,30 +1,32 @@
 
 /**
 * Package for calculate task.
-*
 * @author Sergey Gavrilov (mailto:sarmexin@gmail.com)
 * @version $Id$
 * @since 0.1
 */
-
-
 package ru.job4j.calculate;
 
-public class Calculate{
+/**
+* Class Calculate класс.
+* @autor Sergey Gavrilov (mailto:sarmexin@gmail.com)
+* @since 10.08.2018
+*/
+public class Calculate {
+
       /**
-       * Main.
-       * @param args - args.
-       */
-	
-	public static void main(String[] args){
+      * Main.
+      * @param args - args.
+      */
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 
         /**
-         * Method echo.
-         * @param name Your name.
-         * @return Echo plus your name.
-         */
+        * Method echo.
+        * @param name Your name.
+        * @return Echo plus your name.
+        */ 
         public String echo(String name) {
             return "Echo, echo, echo : " + name;
     }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Sergey Gavrilov (sarmexin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j;

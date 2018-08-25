c2eb1081abee42d40c846f760b2a7b4c457d12cf
sarmexin <mailto:sarmexin@gmail.com>
2018-08-25 15:56:51 +0300

    6.8. Удаление дубликатов в массиве.

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+/**
+ * ArrayDuplicate.
+ *
+ * @author Sergey Gavrilov (sarmexin@gmail.com)
+ * @version $Id$
+ * @since 0.2
+ */
+public class ArrayDuplicate {
+    /**
+     * Method remove.
+     *
+     * @param array.
+     * @return array.
+     */
+    public String[] remove(String[] array) {
+        String time;
+        int number = 0;
+        for (int index = 0; index < array.length - number; index++) {
+            for (int index2 = 0; index2 < array.length - 2 - number; index2++) {
+                if (index == index2) {
+                    index2++;
+                }
+                if (array[index].equals(array[index2])) {
+                    time = array[array.length - 1 - number];
+                    array[array.length - 1 - number] = array[index];
+                    array[index2] = time;
+                    number++;
+                }
+            }
+        }
+        return Arrays.copyOf(array, array.length - number);
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayDuplicateTest {
+    /**
+     * Test remove.
+     */
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] input = new String[]{"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] expect = new String[]{"Привет", "Мир", "Супер"};
+        String[] result = duplicate.remove(input);
+        assertThat(result, is(expect));
+    }
+}

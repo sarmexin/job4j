0b546cf1ad7d35693c5b4dbd115d38fa2a4f4a80
sarmexin <mailto:sarmexin@gmail.com>
2018-09-05 15:08:26 +0300

    Находит максимальное из трёх чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,29 +1,33 @@
 package ru.job4j.max;
 
 /**
+ * Max.
+ *
  * @author Sergey Gavrilov (mailto:sarmexin@gmail.com)
  * @version $Id$
  * @since 0.11
  */
- public class Max {
-	 
-	/**
-    * Находит максимальное из двух чисел.
-    * @param first second Принимает два числа.
-    * @return максимальное число.
-    */
-	public int max(int first, int second) {
-	    return first > second ? first : second;
-	}
-	
+public class Max {
+
     /**
-    * Находит максимальное из трёх чисел.
-    * @param first second third Принимает три числа.
-    * @return максимальное число.
-    */
-	public int maxx(int first, int second, int third) {
-		int temp = this.max(first, second);
-		int tempmax = this.max(temp, third);
-		return tempmax;
-	}	
+     * Находит максимальное из двух чисел.
+     *
+     * @param first second Принимает два числа.
+     * @return максимальное число.
+     */
+    public int max(int first, int second) {
+
+        return first > second ? first : second;
+    }
+
+    /**
+     * Находит максимальное из трёх чисел.
+     *
+     * @param first second third Принимает три числа.
+     * @return максимальное число.
+     */
+    public int maxx(int first, int second, int third) {
+        int temp =  this.max(this.max(first, second), third);
+        return temp;
+    }
 }

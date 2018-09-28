947cd5d7ccc194da55fad0ac1f11693e3f8550d7
sarmexin <mailto:sarmexin@gmail.com>
2018-09-28 15:26:51 +0300

    исправление

--- chapter_002/src/main/java/ru/job4j/chess/Logic.java
+++ chapter_002/src/main/java/ru/job4j/chess/Logic.java
@@ -3,12 +3,10 @@ package ru.job4j.chess;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
-import java.util.Optional;
-
 /**
  * //TODO add comments.
  *
- * @author Sergey gavrilov (sarmexin@gmail.com)
+ * @author Sergey gavrilov (parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
  */

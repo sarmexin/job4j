b081ff48aaac0d6a01fa705d8309278f197e2bd0
sarmexin <mailto:sarmexin@gmail.com>
2018-10-16 16:34:22 +0300

    Телефонный справочник

---
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,40 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sergey gavrilov (sarmexin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<Person>();
+
+    /**
+     * Добавление елемента списка телефонного справочника.
+     *
+     * @param person
+     */
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Поиск совпадений по ключу в телефонном справочнике.
+     *
+     * @param key
+     * @return
+     */
+    public List<Person> find(String key) {
+        List<Person> result = new ArrayList<>();
+        for (int i = 0; i != persons.size(); i++) {
+            System.out.println(i);
+            Person element = persons.get(i);
+            if (element.getName().contains(key) || element.getSurname().contains(key) || element.getAddress().contains(key) || element.getPhone().contains(key)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Sergey Gavrilov (mailto:sarmexin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PhoneDictionaryTest {
+    /**
+     * Проверка на нахождение совпадений в телефонном справочнике.
+     * Test find
+     */
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        List<Person> persons = phones.find("Petr");
+        assertThat(persons.iterator().next().getSurname(), is("Arsentev"));
+    }
+}

b074b927b033794ae54be2992fe1530f43f2e1ac
sarmexin <mailto:sarmexin@gmail.com>
2018-11-19 16:55:19 +0300

    iterator

---
+++ chapter_2001/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -0,0 +1,62 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIterator implements Iterator<Integer> {
+    private int[][] array;
+    private int column = 0;
+    private int line = 0;
+    private boolean flag = true;
+
+    public MatrixIterator(int[][] array) {
+        this.array = array;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean result = false;
+        if (flag && array.length > line) {
+            result = true;
+        }
+        if (line < array.length) {
+            if (column < array[line].length) {
+                result = true;
+            }
+        }
+        if (line < array.length) {
+            if (column + 1 == array[line].length) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Integer next() {
+        int length = array[line].length;
+        if (hasNext()) {
+            if (flag) {
+                column = 0;
+            }
+            if (length == 1) {
+                flag = true;
+            }
+            if (column + 1 == length) {
+                flag = true;
+            }
+            if (column + 1 < length) {
+                if (flag) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                return array[line++][column];
+            } else {
+                return array[line][column++];
+            }
+        } else {
+            throw new NoSuchElementException("No such element");
+        }
+    }
+}

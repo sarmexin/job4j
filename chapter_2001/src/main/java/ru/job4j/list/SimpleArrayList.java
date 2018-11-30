3954e4cc789b80746d1fad6b3105fa1083bf449c
sarmexin <mailto:sarmexin@gmail.com>
2018-11-30 09:45:47 +0300

    Оболочка над массивом

--- chapter_2001/src/main/java/ru/job4j/list/SimpleArrayList.java
+++ chapter_2001/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -1,14 +1,59 @@
 package ru.job4j.list;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
 /**
  * @param <E>
  * @author Sergey gavrilov (sarmexin@gmail.com)
  * @version $Id$
  * @since 0.1
  */
-public class SimpleArrayList<E> {
-    private int size;
+public class SimpleArrayList<E> implements Iterable<SimpleArrayList.Node> {
+    private int position;
     private Node<E> first;
+    private int modCount = 0;
+    private int size;
+
+    public SimpleArrayList(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public Iterator<Node> iterator() {
+        return new SimpleArrayListTest();
+    }
+
+    private class SimpleArrayListTest implements Iterator<Node> {
+        private int expectedModCount = modCount;
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            boolean result = false;
+            Node<E> result2 = first;
+            for (int i = 0; i < index; i++) {
+                result2 = result2.next;
+            }
+            if (result2 != null) {
+                result = true;
+            }
+            return result;
+        }
+
+        @Override
+        public Node next() {
+            Node<E> result = first;
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException("Контейнер изменён");
+            }
+            for (int i = 0; i == index; i++) {
+                result = result.next;
+            }
+            index++;
+            return result;
+        }
+    }
 
     /**
      * Метод вставляет в начало списка данные.
@@ -16,10 +61,14 @@ public class SimpleArrayList<E> {
      * @param date
      */
     public void add(E date) {
+        if (position == size) {
+            throw new IndexOutOfBoundsException("Контейнер заполнен полностью");
+        }
         Node<E> newLink = new Node<>(date);
         newLink.next = this.first;
         this.first = newLink;
-        this.size++;
+        this.position++;
+        modCount++;
     }
 
     /**
@@ -30,11 +79,11 @@ public class SimpleArrayList<E> {
     public boolean delete(E date) {
         Node<E> node = this.first;
         boolean result = false;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < position; i++) {
             if (node.next.date.equals(date)) {
                 node.next = node.next.next;
                 result = true;
-                size--;
+                position--;
                 break;
             }
         }
@@ -53,12 +102,29 @@ public class SimpleArrayList<E> {
     }
 
     /**
-     * Метод получения размера коллекции.
+     * Метод получения размера заполненной коллекции.
      *
      * @return
      */
     public int getSize() {
-        return this.size;
+        return this.position;
+    }
+
+    /**
+     *
+     * @param index
+     * @param model
+     */
+    public void set(int index, E model) {
+        if (index >= 0 && index < size) {
+            Node<E> node = this.first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            node.date = model;
+        } else {
+            throw new IndexOutOfBoundsException("Выход за границы контейнера");
+        }
     }
 
     /**
@@ -66,14 +132,11 @@ public class SimpleArrayList<E> {
      *
      * @param <E>
      */
-    private static class Node<E> {
+    public static class Node<E> {
         E date;
         Node<E> next;
-
         Node(E date) {
             this.date = date;
         }
     }
-
-
 }
--- chapter_2001/src/test/java/ru/job4j/list/SimpleArrayListTest.java
+++ chapter_2001/src/test/java/ru/job4j/list/SimpleArrayListTest.java
@@ -3,6 +3,10 @@ package ru.job4j.list;
 import org.junit.Test;
 import org.junit.Before;
 
+import java.util.ConcurrentModificationException;
+import java.lang.IndexOutOfBoundsException;
+import java.util.Iterator;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -18,7 +22,7 @@ public class SimpleArrayListTest {
 
     @Before
     public void beforeTest() {
-        list = new SimpleArrayList<>();
+        list = new SimpleArrayList<>(4);
         list.add(1);
         list.add(2);
         list.add(3);
@@ -26,7 +30,7 @@ public class SimpleArrayListTest {
 
     @Test
     public void whenAddThreeElementsThenUseGetOneResultTwo() {
-        assertThat(list.get(1), is(2));
+        assertThat(list.get(0), is(3));
     }
 
     @Test
@@ -46,6 +50,39 @@ public class SimpleArrayListTest {
         list.delete(555);
         assertThat(list.getSize(), is(3));
     }
+    @Test
+    public void testIterator() {
+        Iterator iterator = list.iterator();
+        assertThat(iterator.hasNext(), is(true));
+        iterator.next();
+        assertThat(iterator.hasNext(), is(true));
+        iterator.next();
+        assertThat(iterator.hasNext(), is(true));
+        iterator.next();
+        assertThat(iterator.hasNext(), is(false));
+    }
+    @Test(expected = ConcurrentModificationException.class)
+    public void testConcurrentModificationException() {
+        Iterator iterator = list.iterator();
+        list.add(10);
+        assertThat(iterator.hasNext(), is(true));
+        iterator.next();
+    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testArrayIndexOutOfBoundsException() {
+        list.add(10);
+        list.add(11);
+    }
+    @Test
+    public void testSet() {
+        list.set(1, 1000);
+        assertThat(list.get(1), is(1000));
+        list.set(2, 100);
+        assertThat(list.get(2), is(100));
 
-
+    }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetException() {
+        list.set(4, 100);
+    }
 }

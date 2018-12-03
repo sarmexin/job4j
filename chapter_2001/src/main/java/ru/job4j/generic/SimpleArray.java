90a18adb9fb438095b9ee11fbc531ef12c44af42
sarmexin <mailto:sarmexin@gmail.com>
2018-12-03 14:11:48 +0300

    Оболочка над массивом

--- chapter_2001/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_2001/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -6,44 +6,79 @@ public class SimpleArray<T> implements Iterable<T> {
     private final T[] array;
     private int index = 0;
 
-    @Override
-    public Iterator<T> iterator() {
-        return null;
+    public SimpleArray(int cell) {
+        this.array = (T[]) new Object[cell];
     }
 
-    public SimpleArray(int cell) {
-        this.array = (T[]) new Integer[cell];
+    @Override
+    public Iterator<T> iterator() {
+        return new SimpleArrayIterator();
+    }
+
+    public class SimpleArrayIterator implements Iterator<T> {
+        private int index = 0;
+        private int step = 0;
+
+        @Override
+        public boolean hasNext() {
+            boolean result = false;
+            if (index < array.length) {
+                if (array[index] != null) {
+                    result = true;
+                    step = 1;
+                } else {
+                    step = 0;
+                    for (int i = index; i < array.length - index; i++) {
+                        step++;
+                        if (array[index + step] != null) {
+                            index += step;
+                            result = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        @Override
+        public T next() {
+            T result = null;
+            if (this.hasNext()) {
+                result = array[index];
+                index += step;
+            }
+            return result;
+        }
     }
 
     public void add(T model) {
         if (index == array.length) {
             throw new ArrayIndexOutOfBoundsException("Выход за границы массива при добавлении");
-        } else {
-            array[index++] = model;
         }
+        array[index++] = model;
     }
 
     public void set(int index, T model) {
-        if (index >= array.length) {
-            throw new ArrayIndexOutOfBoundsException("Выход за границы массива");
-        } else {
+        if (this.size(index)) {
             array[index] = model;
         }
     }
 
     public void delete(int index) {
-        if (index >= array.length && index >= 0) {
-            throw new ArrayIndexOutOfBoundsException("Выход за границы массива");
-        } else {
+        if (this.size(index)) {
             array[index] = null;
         }
     }
 
     public T get(int index) {
-        if (index >= array.length) {
+        return this.size(index) ? array[index] : null;
+    }
+
+    private boolean size(int index) {
+        if (index >= array.length || index < 0) {
             throw new ArrayIndexOutOfBoundsException("Выход за границы массива");
-        } else {
-            return array[index];
         }
+        return true;
     }
 }
--- chapter_2001/src/test/java/ru/job4j/generic/SimpleArrayTest.java
+++ chapter_2001/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -2,29 +2,54 @@ package ru.job4j.generic;
 
 import org.junit.Test;
 
+import javax.swing.text.html.HTMLDocument;
+
+import java.util.Iterator;
+
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.*;
 
 public class SimpleArrayTest {
+    private SimpleArray<Integer> simpleArray = new SimpleArray<Integer>(20);
+
     @Test
     public void testAdd() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<Integer>(20);
         simpleArray.add(100);
         assertThat(simpleArray.get(0), is(100));
     }
 
     @Test
     public void testSet() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<Integer>(20);
         simpleArray.set(5, 15);
         assertThat(simpleArray.get(5), is(15));
     }
 
     @Test
     public void testDelete() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<Integer>(20);
         simpleArray.set(5, 15);
         simpleArray.delete(5);
-        assertThat(simpleArray.get(5), is(0));
+        assertThat(simpleArray.get(5), is(nullValue()));
+    }
+
+    @Test
+    public void testIterator() {
+        simpleArray.add(10);
+        simpleArray.add(44);
+        simpleArray.add(null);
+        simpleArray.add(null);
+        simpleArray.add(55);
+        Iterator it = simpleArray.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(10));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(44));
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.next(), is(55));
+        assertThat(it.hasNext(), is(false));
+        assertThat(it.next(), is(nullValue()));
+        assertThat(it.next(), is(nullValue()));
+        assertThat(it.hasNext(), is(false));
+        assertThat(it.next(), is(nullValue()));
     }
 }

c632a1f48f2ae4bae44883cb144ed503a910a1ac
sarmexin <mailto:sarmexin@gmail.com>
2018-12-11 10:06:27 +0300

    User

---
+++ chapter_2001/src/main/java/ru/job4j/collections/map/user/Calendar.java
@@ -0,0 +1,5 @@
+package ru.job4j.collections.map.user;
+
+public class Calendar {
+    private int data;
+}
---
+++ chapter_2001/src/main/java/ru/job4j/collections/map/user/User.java
@@ -0,0 +1,7 @@
+package ru.job4j.collections.map.user;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+}

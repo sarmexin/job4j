2329bbcdb407adcec46e1d554103d7049d995fae
sarmexin <mailto:sarmexin@gmail.com>
2018-12-12 08:29:09 +0300

    User

--- chapter_2001/src/main/java/ru/job4j/collections/map/user/Calendar.java
+++ chapter_2001/src/main/java/ru/job4j/collections/map/user/Calendar.java
@@ -1,5 +1,18 @@
 package ru.job4j.collections.map.user;
 
+/**
+ * @author Sergey gavrilov (sarmexin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calendar {
     private int data;
+    private int month;
+    private int year;
+
+    public Calendar(int data, int month, int year) {
+        this.data = data;
+        this.month = month;
+        this.year = year;
+    }
 }
---
+++ chapter_2001/src/main/java/ru/job4j/collections/map/user/Database.java
@@ -0,0 +1,21 @@
+package ru.job4j.collections.map.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sergey gavrilov (sarmexin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Database {
+    static User user1 = new User("Maxim", 1, new Calendar(1, 12, 1976));
+    static User user2 = new User("Maxim", 1, new Calendar(1, 12, 1976));
+    public static void main(String args[]) {
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, "One");
+        map.put(user2, "Two");
+        System.out.println(map.get(user1));
+        System.out.println(map.get(user2));
+    }
+}

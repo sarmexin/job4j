ced5f276c5e9a4e06e7d40e656922b1cc829bb30
sarmexin <sarmexin@gmail.com>
2019-01-18 09:03:10 +0300

    ThreadSafe

--- chapter_2001/src/main/java/threads/threadSafe/DynamicContainerDecorator.java
+++ chapter_2001/src/main/java/threads/threadSafe/DynamicContainerDecorator.java
@@ -3,15 +3,38 @@ package threads.threadSafe;
 import ru.job4j.collections.list.array.DynamicContainer;
 
 import java.util.Iterator;
+
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
 @ThreadSafe
-public abstract class DynamicContainerDecorator<E> implements Iterable<E> {
+public class DynamicContainerDecorator<E> implements Iterable<E> {
     @GuardedBy("this")
-    protected DynamicContainer dynamicContainer;
+    protected DynamicContainer<E> dynamicContainer;
 
-    public DynamicContainerDecorator(DynamicContainer dynamicContainer) {
+    public DynamicContainerDecorator(DynamicContainer<E> dynamicContainer) {
         this.dynamicContainer = dynamicContainer;
+    }
+
+    @Override
+    public synchronized Iterator<E> iterator() {
+        return this.copy(dynamicContainer).iterator();
+    }
+
+    private synchronized DynamicContainer<E> copy(DynamicContainer<E> dynamicContainer) {
+        Object[] array = new Object[dynamicContainer.getIndex()];
+        DynamicContainer<E> container = new DynamicContainer<>(array);
+        for (int i = 0; i < dynamicContainer.getIndex(); i++) {
+            container.add(dynamicContainer.get(i));
+        }
+        return container;
+    }
+
+    public synchronized void add(E value) {
+        dynamicContainer.add(value);
+    }
+
+    public synchronized E get(int index) {
+        return (E) dynamicContainer.get(index);
     }
 }

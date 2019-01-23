c8697fa4654ba83250f26937380374afd00197c1
sarmexin <sarmexin@gmail.com>
2019-01-23 08:00:18 +0300

    Junit test для блокирующей очереди

--- chapter_2001/src/test/java/threads/produserConsumer/SimpleBlockingQueueTest.java
+++ chapter_2001/src/test/java/threads/produserConsumer/SimpleBlockingQueueTest.java
@@ -1,14 +1,12 @@
 package threads.produserConsumer;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.IntStream;
 
-import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 /**
@@ -33,7 +31,6 @@ public class SimpleBlockingQueueTest {
             () -> {
                 while (!queue.getQueue().isEmpty() || !Thread.currentThread().isInterrupted()) {
                     try {
-                        System.out.println("In buffer");
                         buffer.add(queue.poll());
                     } catch (InterruptedException e) {
                         e.printStackTrace();
@@ -42,6 +39,7 @@ public class SimpleBlockingQueueTest {
                 }
             }
     );
+
     @Test
     public void start() {
         producer.start();
@@ -53,36 +51,6 @@ public class SimpleBlockingQueueTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
     }
-
 }
-
-
-//   SimpleBlockingQueue<Integer> simpleBlockingQueue = new SimpleBlockingQueue<>();
-//    Integer[] array1 = new Integer[1000];
-//    Integer[] array2 = new Integer[1000];
-//
-//    @Before
-//    public void before() {
-//        for (int i = 0; i < 1000; i++) {
-//            array1[i] = i;
-//            array2[i] = i + 1000;
-//        }
-//    }
-//
-//    @Test
-//    public void getSize() {
-//        Producer<Integer> producer1 = new Producer<>(simpleBlockingQueue, array1);
-//        Producer<Integer> producer2 = new Producer<>(simpleBlockingQueue, array2);
-//        Consumer<Integer> consumer1 = new Consumer<>(simpleBlockingQueue);
-//        Consumer<Integer> consumer2 = new Consumer<>(simpleBlockingQueue);
-//        try {
-//            consumer1.t.join();
-//            producer1.t.join();
-//            consumer2.t.join();
-//            producer2.t.join();
-//        } catch (InterruptedException e) {
-//            System.out.println("InterruptedException");
-//        }
-//    }

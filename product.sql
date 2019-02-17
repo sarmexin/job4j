7f3292173a80e2fc14aade7cf13fffa08a75f023
sarmexin <sarmexin@gmail.com>
2019-02-17 17:55:36 +0300

    product

--- product.sql
+++ product.sql
@@ -1,13 +1,14 @@
 ﻿create table type (
-	id varchar(2000) primary key
+	id serial primary key,
+	name varchar(2000)
 );
 
 
-drop table type cascade;
+--drop table type cascade;
 insert into type(id ) values('молоко');
 insert into type(id ) values('сыр');
 insert into type(id ) values('мороженое');
-select * from type;
+--select * from type;
 
 
 create table product (
@@ -15,20 +16,21 @@ create table product (
 	name varchar(2000),
 	type_id varchar(2000) references type(id ),
 	expired_date timestamp,
+	number int,
 	price int
 );
 
-drop table product;
-insert into product(name, type_id, expired_date, price) values('адыгейский', 'сыр', '2019-03-14 24:00:00', 500);
-insert into product(name, type_id, expired_date, price) values('российский', 'сыр', '2019-10-14 24:00:00', 700);
-insert into product(name, type_id, expired_date, price) values('голландский', 'сыр', '2019-01-14 24:00:00', 400);
-insert into product(name, type_id, expired_date, price) values('мороженое чистая линия', 'мороженое', '2019-02-20 24:00:00', 70);
-insert into product(name, type_id, expired_date, price) values('мороженое эскимо', 'мороженое', '2019-05-14 24:00:00', 50);
-insert into product(name, type_id, expired_date, price) values('мороженое талосто', 'мороженое', '2019-02-14 24:00:00', 100);
-insert into product(name, type_id, expired_date, price) values('молоко бурёнка', 'молоко', '2019-03-15 24:00:00', 65);
-insert into product(name, type_id, expired_date, price) values('молоко луговое', 'молоко', '2019-03-22 24:00:00', 70);
-insert into product(name, type_id, expired_date, price) values('молоко настоящее', 'молоко', '2019-12-23 24:00:00', 110);
-select * from product;
+--drop table product;
+insert into product(name, type_id, expired_date, number, price) values('адыгейский', 'сыр', '2019-03-14 24:00:00', 100, 500);
+insert into product(name, type_id, expired_date, number, price) values('российский', 'сыр', '2019-10-14 24:00:00', 150, 700);
+insert into product(name, type_id, expired_date, number, price) values('голландский', 'сыр', '2019-01-14 24:00:00', 230, 400);
+insert into product(name, type_id, expired_date, number, price) values('мороженое чистая линия', 'мороженое', '2019-02-20 24:00:00', 9, 70);
+insert into product(name, type_id, expired_date, number, price) values('мороженое эскимо', 'мороженое', '2019-05-14 24:00:00', 55, 50);
+insert into product(name, type_id, expired_date, number, price) values('мороженое талосто', 'мороженое', '2019-02-14 24:00:00', 3, 100);
+insert into product(name, type_id, expired_date, number, price) values('молоко бурёнка', 'молоко', '2019-03-15 24:00:00', 8, 65);
+insert into product(name, type_id, expired_date, number, price) values('молоко луговое', 'молоко', '2019-03-22 24:00:00', 555, 70);
+insert into product(name, type_id, expired_date, number, price) values('молоко настоящее', 'молоко', '2019-12-23 24:00:00', 17, 110);
+--select * from product;
 
 
 
@@ -42,7 +44,7 @@ where u.name like '%мороженое%';
 
 --3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
 select * from product as u
-where u.expired_date between '2019-03-01 00:00:00' and '2019-04-01 00:00:00';
+where u.expired_date < now() + interval '30 day';
 
 --4. Написать запрос, который выводит самый дорогой продукт.
 select max(u.price) from product as u;
@@ -56,9 +58,13 @@ select count(*) from product as u
 where u.type_id = 'сыр' or u.type_id = 'молоко';
 
 --7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.  
-select * from product as u
-where u.expired_date < now() + interval '10 day';
+select (u.type_id) from product as u
+where u.number < 10;
 
 --8. Вывести все продукты и их тип.
-select (c.name, i.id) from type as i
-inner join product as c on i.id = c.type_id;
+select 
+        p.name as product_name,
+        t.id as product_type
+from
+        product p
+        inner join type t on p.type_id = t.id;
